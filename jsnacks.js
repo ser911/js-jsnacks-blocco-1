35e4c0c29992f67f208d26fc3ba777ddf6da5ab7
Sergio <sergio.marretta@libero.it>
2021-03-02 11:20:07 +0100

    end

--- jsnacks.js
+++ jsnacks.js
@@ -1,6 +1,7 @@
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 var playerNumber1 = prompt("Inserisci il primo numero");
 var playerNumber2 = prompt("Inserisci il secondo numero");
+
  if (playerNumber1 > playerNumber2){
    console.log("Il numero vincente è " + playerNumber1)
  }
